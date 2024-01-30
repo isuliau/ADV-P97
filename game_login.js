126b2eacab292b5912657df5ce78d8fbbcb8421a
isuliau <133913055+isuliau@users.noreply.github.com>
2024-01-30 19:11:18 -0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+// Crea la función addUser()
+function  addUser(){
+  // Obtén el valor del usuario con id player1_name_input y player2_name_input
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+  // Almacena los valores en el almacenamiento local
+localStorage.setItem("player1_name", player1_name)
+localStorage.setItem("player2_name", player2_name)
+  //Asigna "game_page.html" a window.location
+  window.location = "game_page.html";
+}
+
